d36cc2206d374cb76a8c3da1b6f5c38b61f9139e
Hesam <hesam.rastegari@gmail.com>
2022-08-06 17:49:26 +0430

    Use a webview to view full article

--- app/src/main/java/com/hessam/newsapiclient/presentation/adapter/NewsAdapter.kt
+++ app/src/main/java/com/hessam/newsapiclient/presentation/adapter/NewsAdapter.kt
@@ -10,12 +10,12 @@ import com.bumptech.glide.Glide
 import com.hessam.newsapiclient.data.model.Article
 import com.hessam.newsapiclient.databinding.NewsListItemBinding
 
-class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
 
     private val callback = object : DiffUtil.ItemCallback<Article>() {
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
-            return  oldItem.url == newItem.url
+            return oldItem.url == newItem.url
         }
 
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
@@ -26,19 +26,17 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
     }
 
 
-
-    val differ = AsyncListDiffer(this,callback)
-
+    val differ = AsyncListDiffer(this, callback)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val binding = NewsListItemBinding
-            .inflate(LayoutInflater.from(parent.context),parent,false)
+            .inflate(LayoutInflater.from(parent.context), parent, false)
         return NewsViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        Log.i("MYTAG","came here2 ${differ.currentList[position].title}")
+        Log.i("MYTAG", "came here2 ${differ.currentList[position].title}")
         val article = differ.currentList[position]
         holder.bind(article)
     }
@@ -48,22 +46,30 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
     }
 
 
-
-
-
-
-    inner class NewsViewHolder(val binding:NewsListItemBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(article: Article){
-            Log.i("MYTAG","came here ${article.title}")
+    inner class NewsViewHolder(val binding: NewsListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+            Log.i("MYTAG", "came here ${article.title}")
             binding.tvTitle.text = article.title
             binding.tvDescription.text = article.description
             binding.tvPublishedAt.text = article.publishedAt
-            binding.tvSource.text = article.source.name
+            binding.tvSource.text = article.source?.name
 
             Glide.with(binding.ivArticleImage.context)
                 .load(article.urlToImage)
                 .into(binding.ivArticleImage)
+
+            binding.root.setOnClickListener{
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
         }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+    fun setOnItemClickListener(listener : (Article)->Unit){
+        onItemClickListener = listener
     }
 }
 
