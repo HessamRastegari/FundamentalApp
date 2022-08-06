5a39f535a1f7718534548c8571cbda773683e8bf
Hesam <hesam.rastegari@gmail.com>
2022-08-06 16:08:24 +0430

    Hilt conflict version fixed Navigation implementation

--- app/src/main/java/com/hessam/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/hessam/newsapiclient/MainActivity.kt
@@ -2,11 +2,26 @@ package com.hessam.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.hessam.newsapiclient.databinding.ActivityMainBinding
+import com.hessam.newsapiclient.presentation.adapter.NewsAdapter
+import com.hessam.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.hessam.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+
+    @Inject
+    lateinit var newsAdapter: NewsAdapter
+
+    lateinit var viewModel: NewsViewModel
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +33,10 @@ class MainActivity : AppCompatActivity() {
         val vanController = navHostFragment.navController
 
         binding.bnvNews.setupWithNavController(vanController)
+
+
+        viewModel = ViewModelProvider(this,factory)
+            .get(NewsViewModel::class.java)
     }
+
 }
